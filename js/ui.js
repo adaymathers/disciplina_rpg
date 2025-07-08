6d810d011a3690727333cb1e2c3da27b7acfcc02
adaymathers <136945677+adaymathers@users.noreply.github.com>
2025-07-08 12:51:47 -0700

    Update ui.js
    Se agrego árbol de habilidades

--- js/ui.js
+++ js/ui.js
@@ -10,6 +10,15 @@ async function cargarDatos() {
   document.getElementById("xpNecesario").textContent = data.xpNecesario;
   document.getElementById("nivel").textContent = data.nivel;
 
+document.getElementById("btnVerHabilidades").addEventListener("click", () => {
+  document.getElementById("contenedorHabilidades").style.display = "block";
+  renderizarArbolDeHabilidades(xpActual); // Asegúrate que esta variable esté disponible
+});
+
+function cerrarArbol() {
+  document.getElementById("contenedorHabilidades").style.display = "none";
+}
+  
   for (let stat in data.stats) {
     document.getElementById(stat).textContent = data.stats[stat];
   }
