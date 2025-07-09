d3e2a13b06aa25a08c0179b4e4cf9b2dec987f5e
adaymathers <136945677+adaymathers@users.noreply.github.com>
2025-07-08 20:15:23 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,12 +4,17 @@ let userId = null;
 document.getElementById("btnLogin").addEventListener("click", login);
 
 async function login() {
-  userId = document.getElementById("username").value.trim();
-  if (!userId) {
+  const nombreUsuario = document.getElementById("username").value.trim();
+
+  if (!nombreUsuario) {
     alert("Pon un nombre de usuario");
     return;
   }
+
+  userId = nombreUsuario;
+  localStorage.setItem("username", userId);
   userDoc = db.collection("usuarios").doc(userId);
+
   const doc = await userDoc.get();
   if (!doc.exists) {
     await userDoc.set({
@@ -17,15 +22,21 @@ async function login() {
       nivel: 1,
       xpNecesario: 100,
       creditos: 0,
-      stats: { fuerza:1, inteligencia:1, sabiduria:1, lenguaje:1, carisma:1 },
+      stats: {
+        fuerza: 1,
+        inteligencia: 1,
+        sabiduria: 1,
+        lenguaje: 1,
+        carisma: 1
+      },
       log: [],
-      ultimaMision: {}
+      actividades: [],
+      misionDiaria: null
     });
   }
+
   document.getElementById("app").style.display = "block";
   document.getElementById("login-container").style.display = "none";
-  const nombreUsuario = document.getElementById("username").value.trim();
-localStorage.setItem("username", nombreUsuario);
 
   cargarDatos();
   cargarMisionDiaria();
